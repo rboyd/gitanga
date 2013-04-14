55beefa63c31ac5d8e59bf067d7e6fba7dc50ea8
Robert Boyd <rboyd@telematter.com>
2013-04-13 21:09:38 -0500

    Adding codec for commit object. Header for object type.

--- src/gitanga/util.clj
+++ src/gitanga/util.clj
@@ -2,7 +2,7 @@
   "Utility functions for gitanga"
   (:require [clojure.java.io :refer [input-stream]]
             [gloss.core :refer [defcodec string repeated ordered-map
-                                string-integer compile-frame]])
+                                string-integer compile-frame header]])
   (:import (java.util.zip InflaterInputStream)) 
   (:import (org.apache.commons.io IOUtils)))
 
@@ -30,11 +30,41 @@
    hexify))
 
 (defcodec tree-entry
-  (ordered-map :mode   (string :utf-8 :delimiters [" "])
-               :name   (string :utf-8 :delimiters ["\0"])
+  (ordered-map :type :tree-entry
+               :mode (string :utf-8 :delimiters [" "])
+               :name (string :utf-8 :delimiters ["\0"])
                :obj-id binary-obj-id))
 
 (defcodec tree
-  (ordered-map :type (string :utf-8 :delimiters [" "])
+  (ordered-map :type :tree
                :length (string-integer :utf-8 :delimiters ["\0"])
                :tree-entries (repeated tree-entry :prefix :none)))
+
+(defn hex-obj-id [type]
+  (compile-frame
+    (string :utf-8 :delimiters ["\n"])
+    identity
+    #(->> (drop (count type) %) (apply str))))
+
+(defcodec commit-parents
+  (repeated (hex-obj-id "parent ") :prefix :none :delimiters ["author "]))
+
+(defcodec commit-contents
+  (ordered-map :type :commit-contents
+               :tree-obj-id (hex-obj-id "tree ")
+               :parents commit-parents
+               :author (string :utf-8 :delimiters ["\ncommitter "])
+               :committer (string :utf-8 :delimiters ["\n\n"])
+               :data (string :utf-8)))
+
+(defcodec commit
+  (ordered-map :type :commit
+               :length (string-integer :utf-8 :delimiters ["\0"])
+               :contents commit-contents))
+
+(defcodec git-obj
+  (header
+   (string :utf-8 :delimiters [" "])
+   {"tree"   tree
+    "commit" commit}
+   identity))
